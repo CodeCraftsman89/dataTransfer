8e64c6bc5ff48377cdb0353932b26bc0a086daed
roman <appleid2505@yandex.ru>
2024-11-19 11:42:03 +0300

    new commit

--- messanger/msng_server.py
+++ messanger/msng_server.py
@@ -4,15 +4,25 @@ from sqlite3 import connect
 HOST = "0.0.0.0"  # The server's hostname or IP address
 PORT = 35533  # The port used by the server
 
+connect = []
+
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
-    print(f"Waiting for connect to {HOST}:{PORT}...")
-    s.listen()
+    while True:
+        print(f"Waiting for connect to {HOST}:{PORT}...")
+        s.listen()
 
-    connect = s.accept()
-    print(f"Connected {connect}")
+        connect.append(s.accept())
+        print(f"Connected {connect}")
 
-    connect[0].close()
-    print(f"Disconnect {connect}")
+        msg = connect[-1][0].recv(1024)
+        print(msg.decode("utf-8"))
+        connect[-1][0].send(msg) #connect[-1][0].send(msg)
+        if msg.decode("utf-8") == "end":
+            break
+
+    for cn in connect:
+        cn[0].close()
+
     s.close()
