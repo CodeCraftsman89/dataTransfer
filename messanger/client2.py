803b15f2f0185fb6dbd52ca46898baeda9b392fb
roman <appleid2505@yandex.ru>
2024-12-01 18:35:46 +0300

    Create protocol, multy users

--- messanger/client2.py
+++ messanger/client2.py
@@ -2,15 +2,29 @@ import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 35533  # The port used by the server
-
+NICK = "BOBA"
+SEP_HEAD = b'/x00'
+SEP_FIELDS = b'/x01'
+CONSTANTS = ["CONN_NICK", "GET_NICK", "SEND", "SEND_ALL", "DISCONN"]
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print(f"Connect to {HOST}:{PORT}")
     s.connect((HOST, PORT))
     print("Connect")
+    msg_nick = CONSTANTS[0].encode("utf-8") + SEP_FIELDS + NICK.encode("utf-8") + SEP_FIELDS +SEP_HEAD
+    s.send(msg_nick)
     while True:
-        user_message = input("Введите сообщение: ")
+        msg_get_all = CONSTANTS[1].encode("utf-8") + SEP_FIELDS + NICK.encode("utf-8") + SEP_FIELDS + SEP_HEAD
+        s.send(msg_get_all)
+        msg = s.recv(1024)
+        print(f"Users: {msg.decode("utf-8")}")
+        msg = input(">>")
+        msg_all = CONSTANTS[3].encode("utf-8") + SEP_FIELDS + NICK.encode("utf-8") +SEP_FIELDS + SEP_HEAD + msg.encode("utf-8")
+        s.send(msg_all)
+        msg = s.recv(1024)
+        '''user_message = input("Введите сообщение: ")
         s.send(user_message.encode("utf-8"))
-        user_message = s.recv(1024)
-        if user_message.decode("utf-8") == "end":
+        user_message = s.recv(1024)'''
+        print(f"Message: {msg.decode("utf-8")}")
+        if msg.decode("utf-8") == "end":
             break
--- messanger/msng_server.py
+++ messanger/msng_server.py
@@ -1,5 +1,20 @@
 import socket
 from sqlite3 import connect
+from threading import Thread
+
+def connection(client: socket.socket):
+    while True:
+        msg = client.recv(1024)
+        print(msg.decode("utf-8"))
+        msg_d = msg.decode("utf-8")
+        client.send(msg) #connect[-1][0].send(msg)
+        '''if msg_d[-3] == "/":
+            clients.append(msg_d)
+        if msg_d.lower() == "get clients":
+            client.send(msg)'''
+        if msg.decode("utf-8") == "end":
+            client.close()
+            break
 
 HOST = "0.0.0.0"  # The server's hostname or IP address
 PORT = 35533  # The port used by the server
@@ -9,20 +24,22 @@ connect = []
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
+
     while True:
         print(f"Waiting for connect to {HOST}:{PORT}...")
         s.listen()
 
         connect.append(s.accept())
-        print(f"Connected {connect}")
+        print(f"Connected {connect[-1]}")
 
-        msg = connect[-1][0].recv(1024)
-        print(msg.decode("utf-8"))
-        connect[-1][0].send(msg) #connect[-1][0].send(msg)
-        if msg.decode("utf-8") == "end":
-            break
+        #msg = s.recv(1024)
+        #print(msg.decode("utf-8"))
+        #s.send(msg)  # connect[-1][0].send(msg)
 
-    for cn in connect:
-        cn[0].close()
+        t = Thread(target=connection, args=(connect[-1][0],), daemon=True)
+        t.start()
 
-    s.close()
+    #for cn in connect:
+    #    cn[0].close()
+
+    #s.close()
