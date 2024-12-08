a33ba2eb8d9bc966b07aa415e263c9087579a917
CodeCraftsman89 <appleid2505@yandex.ru>
2024-12-08 19:43:22 +0300

    08.12

--- messanger/msng_server.py
+++ messanger/msng_server.py
@@ -8,7 +8,8 @@ CONSTANTS = ["CONN_NICK", "GET_NICKS", "SEND", "SEND_ALL", "DISCONN"]
 connect = {}
 def connection(client: socket.socket, client_addr: tuple, all_clients: dict):
     while True:
-        msg = client.recv(1024)
+        msg = client.recv(2)
+        msg = client.recv(int.from_bytes(msg))
         print(f"Message: {msg.decode('utf-8')}")
         full_pack = msg.split(SEP_HEAD)
         head = [field.decode('utf-8') for field in full_pack[0].split(SEP_FIELDS)]
@@ -19,6 +20,7 @@ def connection(client: socket.socket, client_addr: tuple, all_clients: dict):
         elif head[0] == CONSTANTS[1]:
             nicks = "  ".join([k for k in all_clients])
             msg_get_all = CONSTANTS[1].encode('utf-8') + SEP_FIELDS +SEP_FIELDS + SEP_HEAD + nicks.encode('utf-8')
+            msg_get_all = len(msg_get_all).to_bytes(2) + msg_get_all
             client.send(msg_get_all)
 
         elif head[0] == CONSTANTS[3]:
@@ -29,21 +31,11 @@ def connection(client: socket.socket, client_addr: tuple, all_clients: dict):
                     break
                 else:
                     msg_all = CONSTANTS[3].encode('utf-8') + SEP_FIELDS + SEP_FIELDS + SEP_HEAD + full_pack[1]
+                    msg_all = len(msg_all).to_bytes(2) + msg_all
                     client.send(msg_all)
-
-
-        '''client.send(msg) #connect[-1][0].send(msg)
-        if msg_d[-3] == "/":
-            clients.append(msg_d)
-        if msg_d.lower() == "get clients":
-            client.send(msg)
-        if msg.decode("utf-8") == "end":
-            client.close()
-            break'''
 
 HOST = "0.0.0.0"  # The server's hostname or IP address
 PORT = 35533  # The port used by the server
-
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -56,14 +48,5 @@ if __name__ == "__main__":
         sock_cli, addr_cli = s.accept()
         print(f"Connected to {(sock_cli, addr_cli)}")
 
-        #msg = s.recv(1024)
-        #print(msg.decode("utf-8"))
-        #s.send(msg)  # connect[-1][0].send(msg)
-
         t = Thread(target=connection, args=(connect[-1][0],), daemon=True)
         t.start()
-
-    #for cn in connect:
-    #    cn[0].close()
-
-    #s.close()
